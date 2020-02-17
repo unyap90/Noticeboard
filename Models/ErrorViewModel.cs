9447f69f4aa13c8967d4953ac61ce721925a6c66
Lim Chee Yeap <limcheeyeap@hotmail.com>
2020-02-17 11:06:00 +0800

    initial code check in

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Noticeboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
